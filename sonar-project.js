bfbcc9e5ad83d5d29847226780bcb3294d193458
Rohit Kasar <68912433+kcrohit@users.noreply.github.com>
2020-08-08 01:40:07 +0530

    addedd url
    http://34.72.85.72/

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://192.168.56.101:9000',
+       serverUrl: 'http://http://34.72.85.72/:9000',
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
