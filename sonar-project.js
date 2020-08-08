b694f114ffb65bac000f3685ba37a4077c0601bd
Rohit Kasar <68912433+kcrohit@users.noreply.github.com>
2020-08-08 16:32:40 +0530

    e
    e

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const scanner = require('sonarqube-scanner');
      scanner({
-       serverUrl: 'http://35.193.140.155/:9000',
+       serverUrl: 'http://35.193.140.155:9000',
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
