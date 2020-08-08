964360476c6ee75ddf442f1db4b6e39fe7fca528
Rohit Kasar <68912433+kcrohit@users.noreply.github.com>
2020-08-08 16:18:36 +0530

    sonar
    change variable sonarScanner

--- sonar-project.js
+++ sonar-project.js
@@ -1,5 +1,5 @@
-const sonarqubeScanner = require('sonarqube-scanner');
-     sonarqubeScanner({
+const scanner = require('sonarqube-scanner');
+     scanner({
        serverUrl: 'http://http://35.193.140.155/:9000',
        options : {
        'sonar.sources': '.',
